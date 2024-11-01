a28a5f92f6105fbc91582ad909bdf6c086233169
ngocviet <lengocviet31983@gmail.com>
2024-11-01 17:05:36 +0700

    first day

--- app/(marketing)/Footer.tsx
+++ app/(marketing)/Footer.tsx
@@ -1,7 +1,61 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
 export const Footer = () => {
   return (
     <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
-      Footer
+      <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image
+            src="/au.svg"
+            alt="Australia"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          Australia
+        </Button>
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image
+            src="/de.svg"
+            alt="Germany"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          Germany
+        </Button>
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image
+            src="/vn.svg"
+            alt="VietNam"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          Viet Nam
+        </Button>
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image
+            src="/ru.svg"
+            alt="Russia"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          Russia
+        </Button>
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image
+            src="/jp.svg"
+            alt="Japan"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          Japan
+        </Button>
+      </div>
     </footer>
   );
 };
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,6 +1,53 @@
 import { Button } from "@/components/ui/button";
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+} from "@clerk/nextjs";
+import { Loader } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <div>This is marketing page!</div>;
+  return (
+    <div className="max-w-[988] mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justify-center p-4 gap-2">
+      <div className="relative w-[240px] h-[240px] lg:w-[300px] lg:h-[300px] mb-8 lg:mb-0">
+        <Image src="main.svg" fill alt="Main" />
+      </div>
+      <div className="flex flex-col items-center gap-y-8">
+        <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
+          Learn, practice, and master new languages with Lingo.
+        </h1>
+        <div className="flex flex-col items-center gap-y-3 max-w-[330px] w-full">
+          <ClerkLoading>
+            <Loader className="h-10 w-10 text-muted-foreground animate-spin" />
+          </ClerkLoading>
+          <ClerkLoaded>
+            <SignedOut>
+              <SignUpButton mode="modal" forceRedirectUrl="/">
+                <Button size="lg" className="w-full" variant="secondary">
+                  Get started
+                </Button>
+              </SignUpButton>
+              <SignInButton mode="modal" forceRedirectUrl="/">
+                <Button size="lg" className="w-full" variant="primaryOutline">
+                  I already have an account
+                </Button>
+              </SignInButton>
+            </SignedOut>
+            <SignedIn>
+              <Link href="/learn" className="w-full">
+                <Button size="lg" className="w-full" variant="secondary">
+                  Continue Learning
+                </Button>
+              </Link>
+            </SignedIn>
+          </ClerkLoaded>
+        </div>
+      </div>
+    </div>
+  );
 }
